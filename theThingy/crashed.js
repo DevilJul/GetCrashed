44fbc4851dae8d138c67e50f5281a618cc71bc5e
haxolotl <haxolotl@localhost.localdomain>
2018-10-27 17:10:29 +0200

    display acceleration data on a webpage, works in cobi

--- theThingy/crashed.js
+++ theThingy/crashed.js
@@ -6,20 +6,27 @@ function deviceOrientationHandler() {
 
 function deviceMotionHandler(e) {
     if (e.acceleration.x > 0.1 || e.acceleration.y > 0.1 || e.acceleration.z > 0.1) {
-        console.log(e.acceleration.x + ',' + e.acceleration.y + ',' + e.acceleration.z);
+        //let sum = Math.abs(e.acceleration.x) + Math.abs(e.acceleration.y) + Math.abs(e.acceleration.z);
+        //console.log('möp ' + sum);
+        document.getElementById('accelX').innerHTML = 'x: ' +e.acceleration.x;
+        document.getElementById('accelY').innerHTML = 'y: ' + e.acceleration.y;
+        document.getElementById('accelZ').innerHTML = 'z: ' + e.acceleration.z;
+
+        document.getElementById('accelGX').innerHTML = 'x: ' + e.accelerationIncludingGravity.x;
+        document.getElementById('accelGY').innerHTML = 'y: ' +e.accelerationIncludingGravity.y;
+        document.getElementById('accelGZ').innerHTML = 'z: ' +e.accelerationIncludingGravity.z;
+
     }
 }
 
 function init() {
-    if (window.DeviceOrientationEvent) {
-        console.log('haz orientation eventz');
-        window.addEventListener('deviceorientation', deviceOrientationHandler, false);
-    }
 
     if (window.DeviceMotionEvent) {
         console.log('haz device motion eventz');
         window.addEventListener('devicemotion', deviceMotionHandler);
         //setTimeout(stopJump, 3*1000);
+    } else {
+        console.log('crash detection is not supported')
     }
 
 }
