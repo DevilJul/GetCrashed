27989b7790b3bc8d60fad116130f5540e0c4462d
Jetterich <alexander.jetten@yahoo.de>
2018-10-27 18:55:26 +0200

    initial com

--- theThingy/crashed.js
+++ theThingy/crashed.js
@@ -1,1 +1,10 @@
 COBI.init('this will all end in tears');
+
+function setCrashed() {
+    let elem = document.getElementById("txtSimCrash");
+    if (elem.style.display === "none") {
+        elem.style.display = "block";
+    } else {
+        elem.style.display = "none";
+    }
+}
