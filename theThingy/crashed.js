7929f7040db77878109f7b537fd6d71035014d7c
Jetterich <alexander.jetten@yahoo.de>
2018-10-27 23:08:47 +0200

    fancy nancy

--- theThingy/crashed.js
+++ theThingy/crashed.js
@@ -1,5 +1,10 @@
 COBI.init('this will all end in tears');
 
+function crash() {
+    setCrashed();
+    countDownStopCall();
+}
+
 function setCrashed() {
     let elem = document.getElementById("txtSimCrash");
     if (elem.style.display === "none") {
@@ -7,4 +12,28 @@ function setCrashed() {
     } else {
         elem.style.display = "none";
     }
+}
+
+function sendSms() {
+    $.ajax({
+        url: 'https://rest.nexmo.com/sms/json',
+        data:'api_key=e8901200\r\napi_secret=R27HlB3Bm3hYcOU1\r\nto=491726003723\r\nfrom="NEXMO"\r\ntext="Hello from Nexmo"',
+        success: function(data){
+            alert(data);
+            console.log("shits been done")
+            //process the JSON data etc
+        }
+    })
+}
+
+function countDownStopCall() {
+    let elem = document.getElementById("progressStop");
+
+    var downloadTimer = setInterval(function(){
+        elem.value --;
+        if(elem.value <= 0) {
+            clearInterval(downloadTimer);
+            sendSms();
+        }
+    },1000);
 }
--- theThingy/testServo.js
+++ theThingy/testServo.js
@@ -1,55 +1,7 @@
 const Tinkerforge = require('./Tinkerforge.js');
 
-let HOST = '172.16.1.104';
-let PORT = 4223;
-let UID = '6QGaJa'; // Change XXYYZZ to the UID of your Servo Brick
-
-let ipcon = new Tinkerforge.IPConnection(); // Create IP connection
-let servo = new Tinkerforge.BrickServo(UID, ipcon); // Create device object
-
-ipcon.connect(HOST, PORT,
-    function (error) {
-        console.log('Error: ' + error);
-    }
-); // Connect to brickd
-// Don't use device before ipcon is connected
-
-ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
-    function (connectReason) {
-        // Configure two servos with voltage 5.5V
-        // Servo 1: Connected to port 0, period of 19.5ms, pulse width of 1 to 2ms
-        //          and operating angle -100 to 100°
-        //
-        // Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95
-        //          to 1.95ms and operating angle -90 to 90°
-        servo.setOutputVoltage(5500);
-
-        servo.setDegree(0, -10000, 10000);
-        servo.setPulseWidth(0, 1000, 2000);
-        servo.setPeriod(0, 19500);
-        servo.setAcceleration(0, 1000); // Slow acceleration
-        servo.setVelocity(0, 65535); // Full speed
-
-        servo.setDegree(5, -9000, 9000);
-        servo.setPulseWidth(5, 950, 1950);
-        servo.setPeriod(5, 20000);
-        servo.setAcceleration(5, 65535); // Full acceleration
-        servo.setVelocity(5, 65535); // Full speed
-
-        servo.setPosition(0, 10000); // Set to most right position
-        servo.enable(0);
-
-        servo.setPosition(5, -9000); // Set to most left position
-        servo.enable(5);
-    }
-);
-
-console.log('Press key to exit');
-process.stdin.on('data',
-    function (data) {
-        servo.disable(0);
-        servo.disable(5);
-        ipcon.disconnect();
-        process.exit(0);
-    }
-);
+curl -X POST https://api.twilio.com/2010-04-01/Accounts/AC29fa7c0cf2cbd883822eafe5d9126e2f/Messages.json \
+    --data-urlencode "From=+14063845438" \
+--data-urlencode "Body=Hallo bichachos" \
+--data-urlencode "To=+491637118036" \
+-u AC29fa7c0cf2cbd883822eafe5d9126e2f:1971b23e10efab3c13bd947f492a5c87
